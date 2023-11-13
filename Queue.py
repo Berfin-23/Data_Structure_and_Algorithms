d705bfc8c3e690e00d86d78d5f23d902961c011e
Berfin-23 <108613729+Berfin-23@users.noreply.github.com>
2023-11-13 15:04:12 +0530

    Queue.py

--- Queue.py
+++ Queue.py
@@ -1,32 +1,32 @@
 class Queue:
     def __init__(self, size):
-        self.queue = [None] * size
+        self.queue = [None] * size #Creates a list based in size entered in line 37
+        #initial values
         self.front = 0
         self.rear = 0
         self.size = size
 
-    def display(self):
-        if self.front == self.rear:
-            print("Queue is empty")
-        else:
-            print(self.queue)
-
-    def enqueue(self):
-        if self.rear == self.size:
+    def enqueue(self, data):
+        if self.size == self.rear:
             print("The queue is full")
         else:
-            value = int(input("Enter a value to enqueue: "))
-            self.queue[self.rear] = value
+            self.queue[self.rear] = data
             self.rear += 1
+            print(f"The data {data} is enqueued")
 
     def dequeue(self):
         if self.front == self.rear:
-            print("Queue is empty")
+            print("The queue is empty")
         else:
+            print(f"The data {self.queue[self.front]} is dequed")
             self.queue[self.front] = None
             self.front += 1
 
-size = int(input("Enter the size of the Queue: "))
+    def display(self):
+        print(self.queue)
+
+
+size = int(input("Enter the size of the queue: "))
 queue = Queue(size)
 print("Menu")
 print("1)Enqueue")
@@ -34,16 +34,18 @@ print("2)Dequeue")
 print("3)Display")
 print("4)Quit")
 while True:
-    inp = int(input("\nEnter your option: "))
-    match inp:
+    choice = int(input("\nEnter your choice: "))
+    #match case is supported in Python 3.10+
+    match choice:
         case 1:
-            queue.enqueue()
+            data = int(input("Enter the data to enqueue: "))
+            queue.enqueue(data)
         case 2:
             queue.dequeue()
         case 3:
             queue.display()
         case 4:
-            print("Quitting")
+            print("Exiting...")
             quit()
         case _:
-            print("Enter a valid option")
+            print("Enter a valid choice")
