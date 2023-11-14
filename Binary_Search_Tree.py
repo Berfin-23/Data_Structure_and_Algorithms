23a9bd6f345d32b94b92b355228e0abb8a3e0068
Berfin-23 <108613729+Berfin-23@users.noreply.github.com>
2023-11-14 10:10:14 +0530

    Binary_Search_Tree.py

--- Binary_Search_Tree.py
+++ Binary_Search_Tree.py
@@ -1,59 +1,60 @@
-class TreeNode:
-    def __init__(self, key):
+class Node:
+    def __init__(self, data):
         self.left = None
+        self.data = data
         self.right = None
-        self.val = key
+
 
 class BinaryTree:
     def __init__(self):
         self.root = None
 
-    def insert(self, root, key):
+    def insert(self, root, data):
         if root is None:
-            return TreeNode(key)
+            return Node(data)
         else:
-            if key < root.val:
-                root.left = self.insert(root.left, key)
+            if data < root.data:
+                root.left = self.insert(root.left, data)
             else:
-                root.right = self.insert(root.right, key)
+                root.right = self.insert(root.right, data)
             return root
 
     def pre_order(self, root):
         if root:
-            print(root.val, end=' ')
+            print(root.data, end=" ")
             self.pre_order(root.left)
             self.pre_order(root.right)
+
+    def in_order(self, root):
+        if root:
+            self.in_order(root.left)
+            print(root.data, end=" ")
+            self.in_order(root.right)
 
     def post_order(self, root):
         if root:
             self.post_order(root.left)
             self.post_order(root.right)
-            print(root.val, end=' ')
-
-    def in_order(self, root):
-        if root:
-            self.in_order(root.left)
-            print(root.val, end=' ')
-            self.in_order(root.right)
+            print(root.data, end=" ")
 
     def level_order(self, root):
         if root is None:
             return
-        queue = []
-        queue.append(root)
+        queue = [root]
         while queue:
             node = queue.pop(0)
-            print(node.val, end=' ')
+            print(node.data, end=" ")
             if node.left:
                 queue.append(node.left)
             if node.right:
                 queue.append(node.right)
 
+
 bt = BinaryTree()
 root = None
-keys = [50, 30, 20, 40, 70, 60, 80]
-for key in keys:
-    root = bt.insert(root, key)
+values = [50, 30, 20, 40, 70, 60, 80]
+for value in values:
+    root = bt.insert(root, value)
 
 print("Pre-order traversal:")
 bt.pre_order(root)
